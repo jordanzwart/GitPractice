ebbe162cb23c481a64fa4f0a544a147805539aea
jordanzwart <ZWART.JORDAN20@GMAIL.COM>
2018-01-23 14:19:06 -0500

    First commit

---
+++ src/NewGitDemo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class NewGitDemo {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner (System.in);
+		
+		String firstName;
+		
+		System.out.println("Please enter first name: ");
+		firstName = scan.next();
+		System.out.println(firstName);
+		
+
+	}
+
+}
